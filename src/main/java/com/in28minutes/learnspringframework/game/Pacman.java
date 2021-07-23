5aa2dceb9cb3f713a394838bfe7b6be03d190e12
atresleche@gmail.com <mish@Michelles-MacBook-Pro.local>
2021-07-23 11:58:06 -0400

    added Pacman and @Primary to configure to Pacman game

--- src/main/java/com/in28minutes/learnspringframework/game/Pacman.java
+++ src/main/java/com/in28minutes/learnspringframework/game/Pacman.java
@@ -4,21 +4,27 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
-public class SuperContraGame implements GamingConsole {
+@Primary
+public class Pacman implements GamingConsole{
+
+	@Override
 	public void up() {
-		System.out.println("up");
+		System.out.println("pacman up!");
 	}
-	
+
+	@Override
 	public void down() {
-		System.out.println("down");
+		System.out.println("pacman down!");
 	}
-	
+
+	@Override
 	public void left() {
-		System.out.println("left");
+		System.out.println("left!");
 	}
-	
+
+	@Override
 	public void right() {
-		System.out.println("right");
+		System.out.println("right!");
 	}
+
 }
